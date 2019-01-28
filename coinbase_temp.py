8273586b7655e64d6f28269bdb3ad49263092046
Jennifer Jenkins <jjnkns@me.com>
2019-01-27 22:11:30 -0500

    Add files via upload

--- coinbase_temp.py
+++ coinbase_temp.py
@@ -1,5 +1,90 @@
 import cbpro
 public_client = cbpro.PublicClient()
+mydict = public_client.get_currencies()
+
+for n in range(0,len(mydict)):
+    print(mydict[n]['id'], mydict[n]['name'],mydict[n]['details']['symbol'])
+    print('------------------------------')
+
+
+# https://www.youtube.com/watch?v=tdmccmKDFFw
+# code borrowed from youtube demo by Flopperam
+import requests
+import json
+response=requests.get('https://api.coinbase.com/v2/prices/BTC-USD/spot')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Spot Price:", price)
+
+response=requests.get('https://api.coinbase.com/v2/prices/ETH-USD/spot')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Spot Price:", price)
+
+response=requests.get('https://api.coinbase.com/v2/prices/LTC-USD/spot')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Spot Price:", price)
+
+response=requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Buy Price:", price)
+
+response=requests.get('https://api.coinbase.com/v2/prices/ETH-USD/buy')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Buy Price:", price)
+
+response=requests.get('https://api.coinbase.com/v2/prices/LTC-USD/buy')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Buy Price:", price)
+
+response=requests.get('https://api.coinbase.com/v2/prices/BTC-USD/sell')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Sell Price:", price)
+
+response=requests.get('https://api.coinbase.com/v2/prices/ETH-USD/sell')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Sell Price:", price)
+
+response=requests.get('https://api.coinbase.com/v2/prices/LTC-USD/sell')
+data = response.json()
+currency = data["data"]["base"]
+price = data["data"]["amount"]
+print("Currency:", currency, "Sell Price:", price)
+
+# from coinbase.wallet.client import Client
+
+# #client = Client()
+
+# client = Client(api_key, api_secret, api_version='YYYY-MM-DD')
+
+# currency_code = 'USD'  # can also use EUR, CAD, etc.
+
+# Make the request
+#price = client.get_spot_price(currency=currency_code)
+
+#print('Current bitcoin price in %s: %s' % (currency_code, price.amount))
+
+# from coinbase.wallet.client import Client
+# #client = Client(<api_key>, <api_secret>)
+
+# price = client.get_buy_price(currency_pair = 'BTC-USD')
+
+
+#wsClient.products()
 #print(public_client.get_products())
 
 
@@ -26,10 +111,6 @@ public_client = cbpro.PublicClient()
 # public_client.get_product_24hr_stats('ETH-USD')
 #get_currencies
 #print(public_client.get_currencies())
-mydict = public_client.get_currencies()
 
-for n in range(0,len(mydict)):
-    print(mydict[n]['id'], mydict[n]['name'],mydict[n]['details']['symbol'])
-    print('------------------------------')
 #get_time
 #print(public_client.get_time())
